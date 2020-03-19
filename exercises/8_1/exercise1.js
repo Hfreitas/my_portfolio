09a64096240089fd172cb488b5ac73116b8c8924
Hfreitas <hfreitas86@gmail.com>
2020-03-19 13:57:29 -0300

    Exercício 1 resolvido

--- exercises/8_1/exercise1.js
+++ exercises/8_1/exercise1.js
@@ -1,11 +1,22 @@
 // Crie uma função que receba um número e retorne seu fatorial
 
-let factorial = (num) => {
-  num === 0 || num === 1 ? factorial = 1 : num = 0
-  for( let i = num - 1; i>=1; i--) {
+/*const factorial = (num) => {
+  num === 0 || num === 1 ? factorial = 1 : for( let i = num - 1; i>=1; i--) {
 num *= i;
   }
   return factorial;
+};*/
+
+const factorial = (num) => {
+  if (num === 0) {
+    return 1;
+  } else {
+    let result = num;
+    for (let i = num - 1; i >= 1; i--) {
+      result *= i;
+    }
+    return result;
+  }
 };
 
-console.log(factorial(5))
+console.log(factorial(5));
