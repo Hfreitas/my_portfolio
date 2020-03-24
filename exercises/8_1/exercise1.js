859a7e273188dfb065464cabc59a958d0753581d
Hfreitas <hfreitas86@gmail.com>
2020-03-24 17:35:10 -0300

    Acrescida resolução recursiva e testes unitários

--- exercises/8_1/exercise1.js
+++ exercises/8_1/exercise1.js
@@ -1,22 +1,21 @@
 // Crie uma função que receba um número e retorne seu fatorial
+const assert = require('assert');
 
-/*const FACTORIAL = (num) => {
-  num === 0 || num === 1 ? FACTORIAL = 1 : for( let i = num - 1; i>=1; i--) {
-num *= i;
-  }
-  return FACTORIAL;
-};*/
+// const factorial = (num) => (num === 0) || (num === 1) ? 1 : num * factorial(num-1);
 
-const FACTORIAL = (num) => {
-  if (num === 0) {
-    return 1;
-  } else {
-    let result = num;
-    for (let i = num - 1; i >= 1; i--) {
-      result *= i;
-    }
-    return result;
+
+const factorial = num => {
+  if (num === 0 || num === 1) return 1;
+
+  let result = num;
+  for (let i = num - 1; i >= 1; i--) {
+    result *= i;
   }
+  return result;
 };
 
-console.log(FACTORIAL(5));
+assert.strictEqual(factorial(4), 24);
+assert.strictEqual(factorial(3), 6);
+assert.strictEqual(factorial(2), 2);
+assert.strictEqual(factorial(1), 1);
+assert.strictEqual(factorial(0), 1);
