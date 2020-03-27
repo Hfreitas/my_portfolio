057c2cc32f182b643f4599a71549fbcb7e75dfb7
Hfreitas <hfreitas86@gmail.com>
2020-03-27 19:36:06 -0300

    resolucao exercicio 2

--- exercises/9_2/exercises/9_1/exercises9_2.js
+++ exercises/9_2/exercises/9_1/exercises9_2.js
@@ -1,7 +1,5 @@
 const assert = require('assert');
 
-
-
 const books = [
   {
     id: 1,
@@ -69,6 +67,22 @@ const books = [
 const authorBornIn1947 = () => {
   const authorName = books.find((element) => element.author.birthYear === 1947);
   return authorName.author.name;
-}
+};
 
 assert.equal(authorBornIn1947(), 'Stephen King');
+
+// 2. Retorne o nome do livro de menor nome.
+const smallerName = () => {
+  books.sort((a, b) => {
+    const firstBookNameLength = a.name.length;
+    const secondBookNameLength = b.name.length;
+
+    if (firstBookNameLength > secondBookNameLength) return 1;
+    if (firstBookNameLength < secondBookNameLength) return -1;
+    return 0;
+  });
+  return books[0].name;
+  undefined;
+};
+
+assert.equal(smallerName(), 'Duna');
