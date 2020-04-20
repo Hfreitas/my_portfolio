81899eb06c3f2bf42db8b29122ce4c68b60fdc78
Hfreitas <hfreitas86@gmail.com>
2020-04-20 14:57:14 -0300

    Refatorando exercícios com utilização de higher order functions

--- exercises/8_1/exercise1.js
+++ exercises/8_1/exercise1.js
@@ -1,18 +1,19 @@
 // Crie uma função que receba um número e retorne seu fatorial
 const assert = require('assert');
 
-// const factorial = (num) => (num < 2) ? 1 : num * factorial(num-1);
+//Refatoração usando recursividade 
+const factorial = num => (num < 2) ? 1 : num * factorial(num-1);
 
 
-const factorial = num => {
-  if (num === 0 || num === 1) return 1;
+// const factorial = num => {
+//   if (num === 0 || num === 1) return 1;
 
-  let result = num;
-  for (let i = num - 1; i >= 1; i--) {
-    result *= i;
-  }
-  return result;
-};
+//   let result = num;
+//   for (let i = num - 1; i >= 1; i--) {
+//     result *= i;
+//   }
+//   return result;
+// };
 
 assert.strictEqual(factorial(4), 24);
 assert.strictEqual(factorial(3), 6);
--- exercises/8_1/exercise2.js
+++ exercises/8_1/exercise2.js
@@ -15,19 +15,16 @@ const assert = require('assert');
   return longestWord;
 }*/
 
-const longestWord = str => {
-  const arrayOfString = str.split(' ');
-  let characterCount = 0;
-  let longestWord = '';
-  for (let i = 0; i < arrayOfString.length; i++) {
-    if (arrayOfString[i].length > characterCount) {
-      characterCount = arrayOfString[i].length;
-      longestWord = arrayOfString[i];
-    }
-  }
-  return longestWord;
+// Refatoração usando ES6 e higher order functions
+const longestWord = string => {
+  const arrayOfString = string.split(' ');
+  return arrayOfString.reduce(
+    (element, previousElement) =>
+      element.length > previousElement.length ? element : previousElement,
+    '',
+  );
 };
 
-const param = 'Eu sou um desenvolvedor'
+const param = 'Eu sou um desenvolvedor';
 
 assert.strictEqual(longestWord(param), 'desenvolvedor');
