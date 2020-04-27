db30feb4dcae2d0c432a017798235ddfb2812a6e
Hfreitas <hfreitas86@gmail.com>
2020-04-27 12:23:27 -0300

    implementado gerenciamento de estado de PokemonCard

--- exercises/13_1/my-first-pokedex/src/App.js
+++ exercises/13_1/my-first-pokedex/src/App.js
@@ -1,20 +1,43 @@
 /* eslint-disable no-undef */
 import React from 'react';
 import pokemons from './data';
-import PokemonGrid from './components/card-grid';
+// import PokemonGrid from './components/CardGrid';
+import PokemonCard from './components/PokemonCard';
+import Button from './components/Button';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="dex-header">
-        <h1>Vanilla PokeDex</h1>
-      </header>
-      <div>
-        <PokemonGrid key={pokemons.name} pokemons={pokemons} />
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      index: 0,
+    };
+  }
+
+  updateState = () => {
+    this.setState((state) => {
+      //Por que só roda com length -1
+      return state.index < pokemons.length -1
+        ? { index: state.index + 1 }
+        : { index: this.index = 0 };
+    });
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <header className="dex-header">
+          <h1>Vanilla PokeDex</h1>
+        </header>
+        <div>
+          {/* <PokemonGrid key={pokemons.name} pokemons={pokemons} /> */}
+          <PokemonCard pokemons={pokemons[this.state.index]} />
+        </div>
+        <Button handleClick={this.updateState} label="Botão 1" />
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default App;
