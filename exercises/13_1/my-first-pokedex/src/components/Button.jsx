a1acf0bc1033666da11c66b16d8ac74c806c28cd
Hfreitas <hfreitas86@gmail.com>
2020-04-27 12:31:24 -0300

    Alterações nos componentes Button, CardGrid e Pokemoncard

---
+++ exercises/13_1/my-first-pokedex/src/components/Button.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class Button extends React.Component {
+  render() {
+    const { handleClick, label } = this.props;
+    return (
+      <button type="button" onClick={handleClick}>
+        {label}
+      </button>
+    );
+  }
+}
+
+export default Button;
