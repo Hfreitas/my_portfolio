bf4da3d9001381ba5919781a8e4eb84abfcd21cc
Hfreitas <hfreitas86@gmail.com>
2020-05-24 20:56:21 -0300

    commit inicial, setup do projeto

---
+++ portfolio-project/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hebert Freitas Desenvolvedor Web</h1>
+    </div>
+  );
+}
+
+export default App;
